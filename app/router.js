08a459d30057170204d526c885af09655936d9c6
jeffreybiles <bilesjeffrey@gmail.com>
2016-01-24 00:01:03 -0600

    Barebones monster show

--- app/router.js
+++ app/router.js
@@ -6,7 +6,12 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('monsters', {path: '/'})
+  this.route('monsters', function(){
+    this.route('monster', {path: ':monster_id'}, function(){
+      this.route('show')
+      this.route('edit')
+    })
+  })
 });
 
 export default Router;
