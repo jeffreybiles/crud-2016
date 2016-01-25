c936ef69c69fcf80e936bd977cc082b9a121c2c9
jeffreybiles <bilesjeffrey@gmail.com>
2016-01-25 03:13:28 -0600

    save on the component

--- app/components/monster-form.js
+++ app/components/monster-form.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    save(){
+      // grabs the 'save' method off the attrs, calls it
+      // you could chain stuff on it if you wanted, since it will return a promise
+      this.attrs.save()
+    }
+  }
 });
