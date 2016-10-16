90ccd74bfc8fdaf810ff5291032706c257fbcbc1
jeffreybiles <bilesjeffrey@gmail.com>
2016-10-16 16:53:43 -0500

    192- second and third acceptance tests

--- tests/acceptance/monster-index-test.js
+++ tests/acceptance/monster-index-test.js
@@ -4,12 +4,20 @@ import moduleForAcceptance from 'crud-2016/tests/helpers/module-for-acceptance';
 moduleForAcceptance('Acceptance | monster index');
 
 test('visiting /monsters', function(assert) {
-  server.createList('monster', 10);
-  
+  let monsters = server.createList('monster', 10);
+
   visit('/monsters');
 
   andThen(function() {
     assert.equal(currentURL(), '/monsters');
     assert.equal(find('.monster').length, 10);
+    assert.equal(find('.monster .test-monster-name:first').text(), monsters[0].name);
+  });
+
+  click('.monster:first');
+
+  andThen(function(){
+    assert.equal(currentURL(), '/monsters/1/show');
+    assert.equal(find('.name').text(), monsters[0].name);
   });
 });
